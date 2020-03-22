854622b0d4a93a521e809f778322236d9f38f876
SirBlurry <julian_master117@hotmail.com>
2020-03-21 21:59:36 -0600

    ProyectoComputoGrafico
    Sacrificio para Adidier.

---
+++ StudySDL/Utils.cpp
@@ -0,0 +1,24 @@
+/*
+ * Utils.cpp
+ *
+ *  Created on: Jan. 8, 2019
+ *      Author: serge
+ */
+
+#include "Utils.h"
+#include <cmath>
+
+bool IsEqual(float x, float y)
+{
+	return fabsf(x - y) < EPSILON;
+}
+
+bool IsGreaterThanOrEqual(float x, float y)
+{
+	return x > y || IsEqual(x, y);
+}
+
+bool IsLessThanOrEqual(float x, float y)
+{
+	return x < y || IsEqual(x, y);
+}
